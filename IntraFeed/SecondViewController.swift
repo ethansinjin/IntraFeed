b3ed71e7785b7d34a18bdea1e830028af70b7d8b
Ethan Gill <ethan.gill@me.com>
2015-11-21 10:17:33 -0500

    Initial Commit

---
+++ IntraFeed/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  IntraFeed
+//
+//  Created by Ethan Gill on 11/21/15.
+//  Copyright © 2015 Ethan Gill. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
